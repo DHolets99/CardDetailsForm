818ba6ca0792f6bc38e2225326e1435b921d6b16
DHolets99 <dholets99@gmail.com>
2022-08-29 22:54:05 +0300

    29.08 22.54

--- js/jquery/validate.js
+++ js/jquery/validate.js
@@ -1,6 +1,10 @@
 const inputs = document.querySelectorAll('input');
 const form = document.querySelector('form');
+const cardHolderName = document.getElementById('cardHolderName');
 const cardNumber = document.getElementById('cardNumber');
+const month = document.getElementById('date--month');
+const year = document.getElementById('date--year');
+const cvc = document.getElementById('cvc');
 
 const addStartFocus = () => {
     inputs[0].focus();
@@ -30,11 +34,32 @@ const checkEmptyFields = (event) => {
     })
 }
 
+const setMasks = () => {
+    cardHolderNameMask = IMask(cardHolderName, {
+        mask: /^([а-яё\s]+|[a-z\s]+)$/iu,
+        prepare: function (str) {
+            return str.toUpperCase();
+        },
+    })
+    cardNumberMask = IMask(cardNumber, {
+        mask: '0000 0000 0000 0000'
+    })
+    monthMask = IMask(month, {
+        mask: IMask.MaskedRange,
+        from: 1,
+        to: 12,
+        maxLength: 2,
+        autofix: true
+    })
+    yearMask = IMask(year, {
+        mask: '00'
+    })
+    cvcMask = IMask(cvc, {
+        mask: '000'
+    })
+}
 
-const maskOptions = {
-  mask: '0000 0000 0000 0000'
-};
-let mask = IMask(cardNumber, maskOptions);
 
 window.addEventListener('load', addStartFocus);
 form.addEventListener('submit', checkEmptyFields);
+window.addEventListener('load', setMasks);
